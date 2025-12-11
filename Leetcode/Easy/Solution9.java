7f19ca595391e30464d3d31242454ee372631255
vhmaaadhav <vhmaadhav@hotmail.com>
2025-12-11 22:17:27 +0530

    Implement isPalindrome method in Solution9 class

--- Leetcode/Easy/Solution9.java
+++ Leetcode/Easy/Solution9.java
@@ -1,4 +1,20 @@
 package Leetcode.Easy;
 
 public class Solution9 {
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        if (x < 10) {return true;}
+
+        int temp = x;
+        int reverse = 0;
+
+        while (temp > 0) {
+            reverse = reverse * 10 + temp % 10;
+            temp /= 10;
+        }
+
+        return x == reverse;
+    }
 }
