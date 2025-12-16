1e0098f8baa916645a520670241db47d470a35b5
vhmaaadhav <vhmaadhav@hotmail.com>
2025-12-16 11:44:34 +0530

    Implement findrange and binarysearch methods in infinitenumbers; add searchRange and search methods in Solution34; implement peakIndexInMountainArray in Solution852

--- Kunal/infinitenumbers.java
+++ Kunal/infinitenumbers.java
@@ -1,4 +1,36 @@
 package Kunal;
 
 public class infinitenumbers {
+    public static void main(String[] args){
+        int[] arr = {3, 5, 7, 9, 10, 90, 100, 130, 140, 160, 170};
+        int target = 10;
+        System.out.println(findrange(arr,target));
+    }
+    static int findrange(int[] arr, int target){
+        int start = 0;
+        int end =1;
+        while(target>arr[end]){
+            int newstart = end +1;
+            end = end + (end - start +1)*2;
+            start = newstart;
+        }
+
+        return binarysearch(arr,target,start,end);
+
+    }
+    static int binarysearch(int[] arr, int target, int start, int end){
+        while(start<=end){
+            int mid = start + (end - start)/2;
+            if(arr[mid]>target){
+                end = mid-1;
+            }
+            else if(arr[mid]<target){
+                start = mid+1;
+            }
+            else{
+                return mid;
+            }
+        }
+        return -1;
+    }
 }
--- Leetcode/Easy/Solution34.java
+++ Leetcode/Easy/Solution34.java
@@ -1,4 +1,40 @@
 package Leetcode.Easy;
 
-public class Solution34 {
+public class Solution34{
+    public int[] searchRange(int[] nums, int target){
+        int[] ans = {-1,-1};
+        ans[0] = search(nums,target,true);
+        if(ans[0]!=-1){
+            ans[1] =  search(nums,target,false);
+        }
+
+
+        return ans;
+
+    }
+    int search(int[] nums, int target, boolean findstartindex){
+        int start = 0;
+        int end = nums.length-1;
+        int ans =-1;
+        while (start<=end){
+            int mid = start + (end - start)/2;
+            if(nums[mid]>target){
+                end = mid-1;
+            }
+            else if(nums[mid]<target){
+                start = mid+1;
+            }
+            else{
+                ans = mid;
+                if(findstartindex){
+                    end = mid-1;
+                }
+                else{
+                    start = mid+1;
+                }
+            }
+        }
+        return ans;
+    }
+
 }
