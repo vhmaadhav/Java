ce9f5ccd274bdf99cfd02b0cf50ff594974b6221
maadhav <vhmaadhav@hotmail.com>
2026-06-04 19:24:56 +0530

    Create 162-FindPeakElement

---
+++ 162-FindPeakElement/162-FindPeakElement.java
@@ -0,0 +1,18 @@
+// Last updated: 6/4/2026, 7:24:57 PM
+class Solution {
+    public int findPeakElement(int[] arr) {
+        int start = 0;
+        int end = arr.length-1;
+
+        while(start<end){
+            int mid = start + (end - start)/2;
+            if(arr[mid]>arr[mid+1]){
+                end = mid;
+            }
+            else{
+                start = mid+1;
+            }
+        }
+        return start;
+    }
+}
