5943d12b9537f74c8b0b40de698cc8b24f97b5dd
maadhav <vhmaadhav@hotmail.com>
2026-07-18 11:48:14 +0530

    File updated at 7/18/2026, 11:48:15 AM

--- 2016-MaximumDifferenceBetweenIncreasingElements/2016-MaximumDifferenceBetweenIncreasingElements.java
+++ 2016-MaximumDifferenceBetweenIncreasingElements/2016-MaximumDifferenceBetweenIncreasingElements.java
@@ -1,25 +1,17 @@
-// Last updated: 7/18/2026, 11:45:08 AM
+// Last updated: 7/18/2026, 11:48:15 AM
 1class Solution {
 2    public int maximumDifference(int[] nums) {
-3        
-4        int n = nums.length;
-5        
-6        int max = -1;
-7
-8        for(int i=0; i<n; i++){
-9            for(int j=0; j<n; j++){
-10                if (nums[i] < nums[j] ){
-11                    if(i<j){
-12                        if( (nums[j]-nums[i]) > max){
-13                        
-14                        max = (nums[j]-nums[i]);
-15                    
-16                        }
-17                    } 
-18                }
-19            }
-20        }
-21        return max;
-22        
-23    }
-24}
+3        int min = nums[0];
+4        int ans = -1;
+5
+6        for (int i = 1; i < nums.length; i++) {
+7            if (nums[i] > min) {
+8                ans = Math.max(ans, nums[i] - min);
+9            } else {
+10                min = nums[i];
+11            }
+12        }
+13
+14        return ans;
+15    }
+16}
