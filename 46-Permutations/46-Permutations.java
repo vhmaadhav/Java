49be2af3561a48eaea82cbfb1e2387c3aa548cb6
maadhav <vhmaadhav@hotmail.com>
2026-07-31 12:22:12 +0530

    File updated at 7/31/2026, 12:22:12 PM

--- 46-Permutations/46-Permutations.java
+++ 46-Permutations/46-Permutations.java
@@ -1,30 +1,24 @@
-// Last updated: 7/31/2026, 12:19:45 PM
-1
+// Last updated: 7/31/2026, 12:22:12 PM
+1public class Solution {
 2
-3public class Solution {
-4
-5  public List<List<Integer>> permute(int[] nums) {
-6
-7    List<List<Integer>> resultList = new ArrayList<>();
-8
-9    backtrack(resultList, new ArrayList<>(), nums);
-10    return resultList;
-11  }
-12
-13  private void backtrack(List<List<Integer>> resultList,
-14                         ArrayList<Integer> tempList, int[] nums) {
-15    if (tempList.size() == nums.length) {
-16      resultList.add(new ArrayList<>(tempList));
-17      return;
-18    }
-19
-20    for (int number : nums) {
-21      if (tempList.contains(number))
-22        continue;
-23      tempList.add(number);
-24      backtrack(resultList, tempList, nums);
-25      tempList.remove(tempList.size() - 1);
-26    }
-27  }
-28
-29}
+3  public List<List<Integer>> permute(int[] nums) {
+4   List<List<Integer>> list = new ArrayList<>();
+5   // Arrays.sort(nums); // not necessary
+6   backtrack(list, new ArrayList<>(), nums);
+7   return list;
+8}
+9
+10private void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums){
+11   if(tempList.size() == nums.length){
+12      list.add(new ArrayList<>(tempList));
+13   } else{
+14      for(int i = 0; i < nums.length; i++){ 
+15         if(tempList.contains(nums[i])) continue; // element already exists, skip
+16         tempList.add(nums[i]);
+17         backtrack(list, tempList, nums);
+18         tempList.remove(tempList.size() - 1);
+19      }
+20   }
+21} 
+22
+23}
