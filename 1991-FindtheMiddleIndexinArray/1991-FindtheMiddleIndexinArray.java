e25522cd912ee7790e557519b87342285313e55d
maadhav <vhmaadhav@hotmail.com>
2026-08-06 14:31:01 +0530

    File updated at 8/6/2026, 2:31:01 PM

--- 1991-FindtheMiddleIndexinArray/1991-FindtheMiddleIndexinArray.java
+++ 1991-FindtheMiddleIndexinArray/1991-FindtheMiddleIndexinArray.java
@@ -1,32 +1,24 @@
-// Last updated: 8/6/2026, 2:28:45 PM
+// Last updated: 8/6/2026, 2:31:01 PM
 1class Solution {
 2    public int findMiddleIndex(int[] nums) {
-3        int n = nums.length;
+3        int totalSum = 0;
 4
-5        for (int i = 0; i < n; i++) {
-6            int leftSum = prefixSum(nums, i);
-7            int rightSum = SuffixSum(nums, i);
+5        for (int num : nums) {
+6            totalSum += num;
+7        }
 8
-9            if (rightSum == leftSum) {
-10                return i; 
-11            }
-12        }
-13        return -1;
-14
-15    }
-16    public int prefixSum(int[] nums, int index) {
-17        int sum = 0;
-18        for (int i = 0; i < index; i++) {
-19            sum += nums[i];
-20        }
-21        return sum;
+9        int leftSum = 0;
+10
+11        for (int i = 0; i < nums.length; i++) {
+12            int rightSum = totalSum - leftSum - nums[i];
+13
+14            if (leftSum == rightSum) {
+15                return i;
+16            }
+17
+18            leftSum += nums[i];
+19        }
+20
+21        return -1;
 22    }
-23
-24    public int SuffixSum(int[] nums, int index) {
-25        int sum = 0;
-26        for (int i = nums.length - 1; i > index; i--) {
-27            sum += nums[i];
-28        }
-29        return sum;
-30    }
-31}
+23}
