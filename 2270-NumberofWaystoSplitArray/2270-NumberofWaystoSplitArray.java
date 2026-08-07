20865875972b5aec39b1dfde0400e70b04f18e2a
maadhav <vhmaadhav@hotmail.com>
2026-08-07 11:25:17 +0530

    File updated at 8/7/2026, 11:25:18 AM

--- 2270-NumberofWaystoSplitArray/2270-NumberofWaystoSplitArray.java
+++ 2270-NumberofWaystoSplitArray/2270-NumberofWaystoSplitArray.java
@@ -1,19 +1,25 @@
-// Last updated: 8/7/2026, 11:22:32 AM
+// Last updated: 8/7/2026, 11:25:18 AM
 1class Solution {
 2    public int waysToSplitArray(int[] nums) {
-3        long count = 0;
-4        long left = 0;
-5        long total = 0;
-6        for(int n: nums){
-7            total += n;
-8        }
-9        for(int i=0; i<nums.length-1; i++){
-10            left += nums[i];
-11            if(left>=(total-left)){
-12                count++;
-13            }
+3        long total = 0;
+4
+5        for (int num : nums) {
+6            total += num;
+7        }
+8
+9        long leftSum = 0;
+10        int count = 0;
+11
+12        for (int i = 0; i < nums.length - 1; i++) {
+13            leftSum += nums[i];
 14
-15        }
-16        return (int) count;
-17    }
-18}
+15            long rightSum = total - leftSum;
+16
+17            if (leftSum >= rightSum) {
+18                count++;
+19            }
+20        }
+21
+22        return count;
+23    }
+24}
